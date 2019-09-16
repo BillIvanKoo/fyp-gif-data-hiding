4449145a2064806e8b490eba86d29178a2627831
JQ <jq.choong98@gmail.com>
2019-09-16 22:40:49 +0800

    Updated on how to get GIF delay time and use it as input argv to write into a new GIF file

--- matlab/test_write_gif.m
+++ matlab/test_write_gif.m
@@ -3,5 +3,15 @@
 filepath = 'D:\Monash\FIT3162\GIF collection\levi.gif';
 [gifImage map] = imread(filepath, 'gif' ,'Frames', 'all');
 
+% get information of a file
+% https://www.mathworks.com/help/matlab/ref/imfinfo.html
+info = imfinfo(filepath);
+% info.DelayTime returns the delay time (fps) in hundredths of seconds
+% returned value should be divided by 100 (to make it into seconds)
+ans = info.DelayTime;
+fps = ans / 100;
+
 newimagename = 'D:\Monash\FIT3162\GIF collection\levi2.gif';
-imwrite(gifImage, map, newimagename);
+imwrite(gifImage, map, newimagename,'gif','LoopCount',Inf,'DelayTime',fps);
+%imwrite(A,map,filename,'gif','LoopCount',Inf,'DelayTime',1);
+fprintf("gif generated\n");
