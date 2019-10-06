60dc677c2a0d01747e6280b5a3e222f388c72332
Bill Ivan Kooslarto <kooslarto.bill.ivan@gmail.com>
2019-10-07 00:08:11 +0800

    can find the hiding capacity of gif, hide data using lsb

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GifUploader from './components/GifUploader';
+
+function App() {
+  return (
+    <div className="App">
+      <GifUploader/>
+    </div>
+  );
+}
+
+export default App;
