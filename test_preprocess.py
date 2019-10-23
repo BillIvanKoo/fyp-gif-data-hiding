5597f9739c8368d07b1be4034df7b18520eb4099
JQ <jq.choong98@gmail.com>
2019-10-23 14:29:26 +0800

    added unit testing for preprocess.py

--- test_preprocess.py
+++ test_preprocess.py
@@ -1,16 +1,48 @@
 import unittest
 from preprocess import *
 from gif import Gif
+# Author : Jia Qin Choong
+# Usage : Testing functions in preprocess.py
 
-path = "D:\Monash\FIT3162\GIF collection\circle.gif"
+path = "D:\Monash\FIT3162\GIF collection\levi.gif"
 inputgif = Gif.from_file(path)
 
 class Test_Preprocess(unittest.TestCase):
-    def setUp(self):
-        pass
-
     def test_capacity(self):
-        self.assertGreater(count_available_storage(inputgif), 0, "")
+        # check to see if the count returned a value > 0
+        self.assertGreater(count_available_storage(inputgif), 0, "Failed to check capacity")
+
+    def test_get_lct_index(self):
+        # check to see  if the number of frames are obtained properly
+        indexlist = get_lct_index(inputgif)
+        blocks = inputgif.blocks
+        count = 0
+        for i in range(len(blocks)):
+            if str(blocks[i].block_type) == "BlockType.local_image_descriptor":
+                count += 1
+        self.assertEqual(len(indexlist), count, "Failed to get lct index list")
+
+    def test_setlocalCT(self):
+        # check if all the local color table flag has been set to 135
+        indexlist = set_local_color_table(inputgif)
+        count = 0
+        blocks = inputgif.blocks  # the frames are stored in blocks
+
+        for i in range(1, len(indexlist)):
+            index = indexlist[i]
+            if blocks[index].body.flags == 135:  # more than 0 if there is a local color table
+                count += 1
+        # len - 1 because the first frame will not be changed or checked for
+        self.assertEqual(len(indexlist)-1, count, "Failed to set local color table")
+
+    def test_copy_gct(self):
+        table = copy_global_ct(inputgif)
+        self.assertEqual(len(table.entries), 256, "Failed to copy GCT")
+
+    def test_check_gct(self):
+        checkGCT(inputgif)
+        flag = inputgif.logical_screen_descriptor.flags
+        self.assertNotEqual(flag & 128, 0, "Failed check GCT")
 
 if __name__ == '__main__':
     unittest.main()
